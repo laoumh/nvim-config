dcfad6f544b2708356d909adbe1bb4c99d2ff4fe
Lucas Araujo <8809676+laoumh@users.noreply.github.com>
2024-10-15 11:37:21 -0300

    configura sintaxe para arquivos de configuração SSH

--- init.lua
+++ init.lua
@@ -204,6 +204,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Sintaxe de arquivos de configuração SSH
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = '*/.ssh/config.*',
+  command = 'setfiletype sshconfig',
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
