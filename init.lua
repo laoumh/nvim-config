e5228df3963b8e0b49c201d04f482fb282649757
Lucas Araujo <8809676+laoumh@users.noreply.github.com>
2024-10-30 10:09:20 -0300

    Cria comando :H para abrir help em nova aba

--- init.lua
+++ init.lua
@@ -174,6 +174,12 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
 })
 
+-- -- [[ Comandos definidos pelo usuário ]]
+-- `:H` abre `help` em nova aba
+vim.api.nvim_create_user_command('H', function(opts)
+  vim.cmd('tab keepjumps help ' .. opts.args)
+end, { nargs = '*', complete = 'help' })
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
