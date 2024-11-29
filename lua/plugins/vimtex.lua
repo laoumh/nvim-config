448aa8b735d0c3514e2f868c272be2e04d6320b6
Lucas Araujo <8809676+laoumh@users.noreply.github.com>
2024-11-29 11:18:56 -0300

    Vimtex: configura visualizador 'sioyek'

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -3,7 +3,7 @@
 --  - Considerar o LSP (Gramática) https://valentjn.github.io/ltex/index.html
 return {
   "lervag/vimtex",
-  lazy = true,
+  lazy = false,
   ft = "tex",
   dependencies = {
     -- Configuração: https://github.com/micangl/cmp-vimtex/blob/master/doc/TUTORIAL.md
@@ -11,7 +11,11 @@ return {
   },
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
-    -- VimTeX configuration goes here, e.g.
-    vim.g.vimtex_view_method = "zathura"
+    -- Inverse search:
+    --   - No editor: <f4> (toggle_synctec)
+    --   - clique com mouse direito
+    --   - Obs: necessário `lazy = false` para busca inversa funcionar
+    -- Docs: https://sioyek-documentation.readthedocs.io/
+    vim.g.vimtex_view_method = "sioyek"
   end
 }
