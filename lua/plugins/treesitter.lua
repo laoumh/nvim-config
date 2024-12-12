0d751179968969489f6d6d749b0210362fd26419
Lucas Araujo <8809676+laoumh@users.noreply.github.com>
2024-12-12 09:51:23 -0300

    Vimtex: configura which-key conforme wiki
    Ver https://github.com/lervag/vimtex/wiki/which%E2%80%90key.nvim-support

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -17,7 +17,7 @@ return {
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
-      auto_install = true,
+      auto_install = false,
       highlight = {
         enable = true,
         disable = {"latex"},
