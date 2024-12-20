28ba671f49c39205ccd826578d62bff3aa83358b
Lucas Araujo <8809676+laoumh@users.noreply.github.com>
2024-12-20 17:19:10 -0300

    Configura comment.lua

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,5 +1,19 @@
 -- comentário de linha e bloco
 -- Ver https://github.com/numToStr/Comment.nvim?tab=readme-ov-file#configuration-optional para configurações
 return {
-  'numToStr/Comment.nvim',
+  {
+    -- Highlight todo, notes, etc in comments
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false }
+  },
+  {
+    -- Ver :H comment.config para detalhes sobre
+    -- configuração e keymappings
+    'numToStr/Comment.nvim',
+    config = function ()
+      require('Comment').setup()
+    end
+  },
 }
