9adcd5cb5a4685a276693b06144547a373d2498d
Lucas Araujo <8809676+laoumh@users.noreply.github.com>
2024-12-20 17:27:34 -0300

    Traz algumas sugestões de kickstart/plugins

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -6,7 +6,16 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
+        bash = { 'shellcheck' },
         markdown = { 'markdownlint' },
+        sh = { 'shellcheck' },
+        python = {
+          -- "ruff",
+          -- "pycodestyle",
+          -- "pylint",
+        }
+        -- dockerfile = { "hadolint" },
+        -- yaml = { "yamllint" },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
@@ -41,15 +50,41 @@ return {
       -- lint.linters_by_ft['terraform'] = nil
       -- lint.linters_by_ft['text'] = nil
 
-      -- Create autocommand which carries out the actual linting
-      -- on the specified events.
+      -- Set `lint_enabled` to true or false to enable or disable
+      -- the linter by default
+      local lint_enabled = true
       local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-        group = lint_augroup,
-        callback = function()
+      local lint_autocmd = function()
+        vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+          group = lint_augroup,
+          callback = function()
+            lint.try_lint()
+          end,
+        })
+        print("Linter enabled")
+      end
+      if lint_enabled then
+        lint_autocmd()
+      end
+
+      local toggle_linter = function ()
+        if lint_enabled then
+          -- If lint is enable, toggle disables it
+          -- Clear autocommands to stop the linter
+          vim.api.nvim_clear_autocmds({ group = lint_augroup })
+          -- Clear diagnostics to remove virtual text and signs
+          vim.diagnostic.reset(nil, vim.api.nvim_get_current_buf())
+          lint_enabled = false
+          print("Linter disabled")
+        else
+          -- If lint is disabled, toggle enables it
           lint.try_lint()
-        end,
-      })
+          lint_enabled = true
+          lint_autocmd()
+        end
+      end
+
+      vim.keymap.set('n', '<leader>L', toggle_linter, { noremap = true, desc = "Toggle Linter" })
     end,
   },
 }
