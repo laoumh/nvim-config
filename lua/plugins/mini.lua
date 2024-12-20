f7ce118c5336a6ae60cb643a93941df63d0112dc
Lucas Araujo <8809676+laoumh@users.noreply.github.com>
2024-12-20 17:30:09 -0300

    Termina de desmembrar kickstart_default.lua

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,13 +1,4 @@
--- TODO: ver configurações em kickstart/plugins
 return {
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-
-  -- Autoformat
-  -- Se algum dia precisar, ver 'stevearc/conform.nvim' no kickstart.nvim original
-
-    -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
@@ -45,23 +36,4 @@ return {
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
-
-  -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
-  -- init.lua. If you want these files, they are in the repository, so you can just download them and
-  -- place them in the correct locations.
-
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-  --
-  --  Here are some example plugins that I've included in the Kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
 }
