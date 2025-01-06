b29574eb0ed6d3484b19b512cff2fc2e607ead4f
Lucas Araujo <8809676+laoumh@users.noreply.github.com>
2025-01-06 13:47:22 -0300

    Move bootstrap Lazy para arquivo correto

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,14 +1,19 @@
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---  Docs: https://lazy.folke.io/
--- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+--  DOCS:
+--  https://lazy.folke.io/
+--  https://github.com/folke/lazy.nvim
+--  `:help lazy.nvim.txt`
+
+-- Instala `lazy.nvim`, se necessário
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
 require('lazy').setup({
   spec = {
     { import = 'plugins' },
--- lua/settings/autocommands.lua
+++ lua/settings/autocommands.lua
@@ -1,12 +1,10 @@
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
+-- [[ Autocommands ]]
+-- DOCS: `:help lua-guide-autocommands`
 
 -- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
@@ -27,20 +25,8 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
 })
 
--- -- [[ Comandos definidos pelo usuário ]]
 -- `:H` abre `help` em nova aba
 vim.api.nvim_create_user_command('H', function(opts)
   vim.cmd('tab keepjumps help ' .. opts.args)
 end, { nargs = '*', complete = 'help' })
 
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
