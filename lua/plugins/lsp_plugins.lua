8a467f3384d7bd27d74cd3927a097a30a3be776b
Lucas Araujo <8809676+laoumh@users.noreply.github.com>
2025-01-10 09:52:17 -0300

    Move configuração Mason para arquivo próprio

--- lua/plugins/lsp_plugins.lua
+++ lua/plugins/lsp_plugins.lua
@@ -6,11 +6,6 @@ return {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
     dependencies = {
-      -- Automatically install LSPs and related tools to stdpath for Neovim
-      { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
-      'williamboman/mason-lspconfig.nvim',
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
-
       -- Useful status updates for LSP.
       { 'j-hui/fidget.nvim', opts = {} },
 
