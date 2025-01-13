5a75dba3f103c780322327ab85e2e0109e94072d
Lucas Araujo <8809676+laoumh@users.noreply.github.com>
2025-01-13 09:57:48 -0300

    Move spellcheck para settings.lua

--- lua/settings/autocommands.lua
+++ lua/settings/autocommands.lua
@@ -16,15 +16,6 @@ vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
   command = 'setfiletype sshconfig',
 })
 
--- Corretor ortográfico
-vim.api.nvim_create_autocmd('FileType', {
-  desc = 'Habilita corretor ortográfico em arquivos markdown',
-  callback = function()
-    vim.opt_local.spell = true
-    vim.opt_local.spelllang = 'en_us,pt_br'
-  end,
-})
-
 -- `:H` abre `help` em nova aba
 vim.api.nvim_create_user_command('H', function(opts)
   vim.cmd('tab keepjumps help ' .. opts.args)
