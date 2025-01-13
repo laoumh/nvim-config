0a548e1bd355060a87b76e66a457a36539a6f535
Lucas Araujo <8809676+laoumh@users.noreply.github.com>
2025-01-13 09:18:52 -0300

    Verifica se LSP suporta método para adicionar atalho

--- lua/plugins/lsp_plugins.lua
+++ lua/plugins/lsp_plugins.lua
@@ -22,47 +22,78 @@ return {
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
         callback = function(event)
-
           local tsbuiltin = require('telescope.builtin')
+          local client = vim.lsp.get_client_by_id(event.data.client_id)
+          local supports_method = function (method)
+            return client and client.supports_method(method)
+          end
+          -- Ver `:help lsp-method`
+          local lsp_methods = vim.lsp.protocol.Methods
+          local fname = vim.api.nvim_buf_get_name(event.buf)
 
           -- [[ CONFIGURA ATALHOS ]]
           local wk = require("which-key")
           wk.add({
-            { "<leader>l", group = "[L]SP", mode = "n" },
+            { "<leader>l", group = "[L]SP", mode = "n", icon = {cat = 'file', name = fname}  },
             {
               mode = "n",
 
               -- Rename the variable under your cursor.
-              { '<leader>lr', vim.lsp.buf.rename, desc = '[R]ename' },
+              { '<leader>lr', vim.lsp.buf.rename, desc = '[R]ename',
+                cond = supports_method(lsp_methods.textDocument_rename)
+              },
 
               -- Execute a code action
-              { '<leader>lc', vim.lsp.buf.code_action, desc = '[C]ode Action', icon = "󰈸", mode = { 'n', 'x' } },
+              { '<leader>lc', vim.lsp.buf.code_action, desc = '[C]ode Action', icon = "󰈸", mode = { 'n', 'x' },
+                cond = supports_method(lsp_methods.textDocument_codeAction)
+              },
+
+              -- Toggle inlay hints. Ex: mostra args de função
+              { '<leader>lh', function()
+                  vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+                end,
+                desc = 'Toggle Inlay [H]ints',
+                cond = supports_method(lsp_methods.textDocument_inlayHint),
+              },
 
               -- GOTO
               { "<leader>lg", group = "[G]oto", icon = ""},
-              { '<leader>lgd', tsbuiltin.lsp_definitions, desc = '[D]efinition' },
+              { '<leader>lgd', tsbuiltin.lsp_definitions, desc = '[D]efinition',
+                cond = supports_method(lsp_methods.textDocument_definition),
+              },
               --  Declaration: in C this would take you to the header, for example
-              { '<leader>lgD', vim.lsp.buf.declaration, desc = '[D]eclaration' },
+              { '<leader>lgD', vim.lsp.buf.declaration, desc = '[D]eclaration',
+                cond = supports_method(lsp_methods.textDocument_declaration),
+              },
               --  Useful when your language has ways of declaring types without an actual implementation.
-              { '<leader>lgi', tsbuiltin.lsp_implementations, desc = '[I]mplementation' },
+              { '<leader>lgi', tsbuiltin.lsp_implementations, desc = '[I]mplementation',
+                cond = supports_method(lsp_methods.textDocument_implementation),
+              },
               -- Jump to the TYPE of the word under your cursor.
-              { '<leader>lgt', tsbuiltin.lsp_type_definitions, desc = '[T]ype Definition' },
+              { '<leader>lgt', tsbuiltin.lsp_type_definitions, desc = '[T]ype Definition',
+                cond = supports_method(lsp_methods.textDocument_typeDefinition),
+              },
 
               -- FIND
               --  Symbols are things like variables, functions, types, etc.
               { "<leader>lf", group = "[F]ind", icon = "󰮗"},
-              { '<leader>lfr', tsbuiltin.lsp_references, desc = '[R]eferences' },
+              { '<leader>lfr', tsbuiltin.lsp_references, desc = '[R]eferences',
+                cond = supports_method(lsp_methods.textDocument_typeDefinition),
+              },
               -- Search this document
-              { '<leader>lfd', tsbuiltin.lsp_document_symbols, desc = '[D]ocument Symbols' },
+              { '<leader>lfd', tsbuiltin.lsp_document_symbols, desc = '[D]ocument Symbols',
+                cond = supports_method(lsp_methods.textDocument_documentSymbol),
+              },
               -- Search whole workspace
-              { '<leader>lfw', tsbuiltin.lsp_dynamic_workspace_symbols, desc = '[W]orkspace Symbols' },
+              { '<leader>lfw', tsbuiltin.lsp_dynamic_workspace_symbols, desc = '[W]orkspace Symbols',
+                cond = supports_method(lsp_methods.workspace_symbol),
+              },
             },
           })
 
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+          if supports_method(lsp_methods.textDocument_documentHighlight) then
             local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
             vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
               buffer = event.buf,
@@ -83,18 +114,6 @@ return {
                 vim.lsp.buf.clear_references()
                 vim.api.nvim_clear_autocmds { group = 'lsp-highlight', buffer = event2.buf }
               end,
-            })
-          end
-
-          -- The following code creates a keymap to toggle inlay hints in your
-          -- code, if the language server you are using supports them
-          --
-          -- This may be unwanted, since they displace some of your code
-          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-            wk.add({
-              { '<leader>lh', function()
-                vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-              end, desc = 'Toggle Inlay [H]ints' }
             })
           end
         end,
