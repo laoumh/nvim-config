c326efd394d9f50bf968d1c004da21755d26fa03
Lucas Araujo <8809676+laoumh@users.noreply.github.com>
2025-01-21 14:10:17 -0300

    Mostra arquivos ocultos em neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -15,6 +15,10 @@ return {
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+      },
       window = {
         position = "float",
         mappings = {
