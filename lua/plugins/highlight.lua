7f0966605226532cc5ab6b9a022fac7031e30601
Lucas Araujo <8809676+laoumh@users.noreply.github.com>
2025-01-21 15:59:35 -0300

    Aplica separador nas janelas e statusline global

--- lua/plugins/highlight.lua
+++ lua/plugins/highlight.lua
@@ -1,3 +1,16 @@
 return {
+  -- Logs highlight
   { 'fei6409/log-highlight.nvim', opts = {}},
+  -- Window separator
+  {
+    "nvim-zh/colorful-winsep.nvim",
+    -- config = true,
+    opts = {
+      hi = {
+        fg = "#4D4941",
+        -- bg = "#0000FF",
+      },
+    },
+    event = { "WinLeave" },
+  },
 }
--- lua/settings/settings.lua
+++ lua/settings/settings.lua
@@ -19,6 +19,9 @@ vim.opt.mouse = ''
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
+-- Global statusline
+vim.opt.laststatus = 3
+
 -- Mostra mensagem se arquivo foi alterado externamente
 vim.opt.autoread = false
 
