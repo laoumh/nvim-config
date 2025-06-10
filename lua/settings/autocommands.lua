e25febd678b4c967eaa6b659fbc9bc86040be980
Lucas Araujo <8809676+laoumh@users.noreply.github.com>
2025-06-10 10:17:39 -0300

    Autocommand para realçar espaços em branco à direita

--- lua/settings/autocommands.lua
+++ lua/settings/autocommands.lua
@@ -21,3 +21,24 @@ vim.api.nvim_create_user_command('H', function(opts)
   vim.cmd('tab keepjumps help ' .. opts.args)
 end, { nargs = '*', complete = 'help' })
 
+-- Realça espaço em branco à direita, mas não quando em modo inserção
+-- Ver `:help highlight-groups` para mais opções de cores
+vim.api.nvim_create_autocmd({"BufEnter", "InsertLeave"}, {
+  desc = "Highlight trailing spaces",
+  group = vim.api.nvim_create_augroup("highlight-trail", { clear = true }),
+  callback = function()
+    vim.schedule(function()
+      if not vim.g.trailing_whitespace_match then
+        vim.g.trailing_whitespace_match = vim.fn.matchadd("Substitute", "\\s\\+$")
+      end
+    end)
+  end,
+})
+vim.api.nvim_create_autocmd({"InsertEnter"}, {
+  desc = "Remove highlight trailing spaces",
+  group = vim.api.nvim_create_augroup("remove-highlight-trail", { clear = true }),
+  callback = function()
+    vim.fn.matchdelete(vim.g.trailing_whitespace_match)
+    vim.g.trailing_whitespace_match = nil
+  end,
+})
